a0cce3c7d4cceeedbf9f6c8f3e405ae8771b72b3
ArdaSabuncu <ardasabuncu17@gmail.com>
2026-05-26 21:14:46 +0300

    Add Industrial class for zone management

---
+++ src/Industrial.java
@@ -0,0 +1,44 @@
+package objectville.cells;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+public class Industrial extends Zone {
+
+    private static final Set<Utility> REQUIRED =
+        EnumSet.of(Utility.ELECTRICITY, Utility.WATER);
+
+    public Industrial(int row, int col) { 
+        super(row, col); 
+    }
+
+    @Override
+    public char getSymbol() {
+        return 'I'; 
+    }
+    @Override 
+    public Set<Utility> getRequiredUtilities() { 
+        return REQUIRED; 
+    }
+
+    @Override
+    protected int computeQualifiedLevel() {
+        
+        if (getReceivedPopulation() <= 0) return 0; 
+        boolean l2 = hasService(Service.SECURITY);
+        boolean l3 = l2 && getReceivedPopulation() > minDeliveredAcrossRequired();
+        if (l3) return 3;
+        if (l2) return 2;
+        return 1;
+    }
+
+    @Override
+    protected int computeOutput(int m) {
+        switch (getLevel()) {
+            case 1: return m;
+            case 2: return 2 * m;
+            case 3: return 2 * m + getReceivedPopulation();
+            default: return 0;
+        }
+    }
+}
