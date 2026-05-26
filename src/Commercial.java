5f203df57ff4280f383372d79ecc8df5faff3741
ArdaSabuncu <ardasabuncu17@gmail.com>
2026-05-26 21:18:22 +0300

    Add Commercial class to manage commercial zones

---
+++ src/Commercial.java
@@ -0,0 +1,45 @@
+package objectville.cells;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+
+public class Commercial extends Zone {
+
+    private static final Set<Utility> REQUIRED =
+        EnumSet.of(Utility.ELECTRICITY, Utility.WATER, Utility.INTERNET);
+
+    public Commercial(int row, int col) {
+      super(row, col); 
+    }
+
+    @Override
+    public char getSymbol() { 
+        return 'C'; 
+    }
+    @Override
+    public Set<Utility> getRequiredUtilities() { 
+        return REQUIRED;
+    }
+
+    @Override
+    protected int computeQualifiedLevel() {
+        if (getReceivedPopulation() <= 0 || getReceivedGoods() <= 0) return 0;
+        boolean l2 = hasService(Service.SECURITY);
+        int m = minDeliveredAcrossRequired();
+        boolean l3 = l2 && getReceivedPopulation() > m && getReceivedGoods() > m;
+        if (l3) return 3;
+        if (l2) return 2;
+        return 1;
+    }
+
+    @Override
+    protected int computeOutput(int m) {
+        switch (getLevel()) {
+            case 1: return m;
+            case 2: return 2 * m;
+            case 3: return 2 * m + Math.min(getReceivedPopulation(), getReceivedGoods());
+            default: return 0;
+        }
+    }
+}
