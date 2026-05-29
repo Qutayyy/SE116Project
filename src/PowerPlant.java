2f3ffa61ada241f07ca6533122ea480ad6199e82
ArdaSabuncu <ardasabuncu17@gmail.com>
2026-05-29 18:59:13 +0300

    Add PowerPlant class extending UtilityProvider

---
+++ src/PowerPlant.java
@@ -0,0 +1,16 @@
+package objectville.cells;
+
+public class PowerPlant extends UtilityProvider {
+    public PowerPlant(int row, int col) {
+        super(row, col); 
+    }
+    @Override
+    public char getSymbol() { 
+        return 'P'; 
+    }
+    @Override
+    public UtilityConsumer.Utility getProducedUtility() {
+        
+        return UtilityConsumer.Utility.ELECTRICITY;
+    }
+}
