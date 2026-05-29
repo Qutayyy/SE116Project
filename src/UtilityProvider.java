d374a004b4c9f88610b701699cc95087daba8092
ArdaSabuncu <ardasabuncu17@gmail.com>
2026-05-29 18:57:42 +0300

    Add abstract UtilityProvider class

---
+++ src/UtilityProvider.java
@@ -0,0 +1,16 @@
+package objectville.cells;
+
+public abstract class UtilityProvider extends Cell {
+
+    private static final int CAPACITY = 100;
+
+    protected UtilityProvider(int row, int col) {
+        super(row, col);
+    }
+
+    public abstract UtilityConsumer.Utility getProducedUtility();
+
+    public int getCapacity() { 
+        return CAPACITY; 
+    }
+}
